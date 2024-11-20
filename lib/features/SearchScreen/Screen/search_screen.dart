35f7950da85941d2dfcf1a0f5610c317da43bcc6
bipul khatiwada <bipul.khatiwada@swifttech.com.np>
2024-11-20 11:03:25 +0545

    empty screen performance enhanced

--- lib/features/SearchScreen/Screen/search_screen.dart
+++ lib/features/SearchScreen/Screen/search_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:media_flow/Widgets/audio_file_display_card.dart';
-import 'package:media_flow/Widgets/empty_screen.dart';
 import 'package:media_flow/bloc/MusicBloc/musicPlayer_bloc.dart';
 import 'package:media_flow/bloc/MusicBloc/musicPlayer_event.dart';
 import 'package:media_flow/bloc/MusicBloc/musicPlayer_state.dart';
+import 'dart:async';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key, required this.title});
@@ -17,6 +17,7 @@ class SearchScreen extends StatefulWidget {
 
 class _SearchScreenState extends State<SearchScreen> {
   late TextEditingController _controller;
+  Timer? _debounceTimer;
 
   @override
   void initState() {
@@ -27,78 +28,139 @@ class _SearchScreenState extends State<SearchScreen> {
   @override
   void dispose() {
     _controller.dispose();
+    _debounceTimer?.cancel();
     context.read<MusicBloc>().add(ClearFilterSongs());
     super.dispose();
+  }
+
+  void _onSearchChanged(String text) {
+    _debounceTimer?.cancel();
+    _debounceTimer = Timer(const Duration(milliseconds: 300), () {
+      context.read<MusicBloc>().add(SearchFileEvent(searchText: text));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false, // Prevent keyboard from pushing content
       appBar: AppBar(
         title: Text(widget.title),
         backgroundColor: Colors.green,
         actions: [
-          SizedBox(
-            width: 280,
-            child: TextField(
-              controller: _controller,
-              autofocus: true,
-              autocorrect: false,
-              onChanged: (text) {
-                context
-                    .read<MusicBloc>()
-                    .add(SearchFileEvent(searchText: text));
-              },
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 64),
+              child: TextField(
+                controller: _controller,
+                autofocus: true,
+                autocorrect: false,
+                decoration: const InputDecoration(
+                  hintText: 'Search...',
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.symmetric(vertical: 12),
+                ),
+                onChanged: _onSearchChanged,
+              ),
             ),
           ),
           IconButton(
             icon: const Icon(Icons.cancel_outlined, color: Colors.black),
             onPressed: () {
-              _controller.text = "";
+              _controller.clear();
+              context.read<MusicBloc>().add(const SearchFileEvent(searchText: ''));
             },
           ),
         ],
       ),
-      body: _buildBody(context),
+      body: SafeArea(
+        child: BlocBuilder<MusicBloc, MusicPlayerState>(
+          buildWhen: (previous, current) =>
+              previous.filteredFileList != current.filteredFileList,
+          builder: (context, state) {
+            final filteredFiles = state.filteredFileList ?? [];
+            return DecoratedBox(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Colors.black, Colors.black54],
+                ),
+              ),
+              child: filteredFiles.isEmpty
+                  ? _buildEmptyState()
+                  : _buildSearchResults(filteredFiles),
+            );
+          },
+        ),
+      ),
     );
   }
 
-  Widget _buildBody(BuildContext context) {
-    return BlocBuilder<MusicBloc, MusicPlayerState>(
-      builder: (context, state) {
-        final filteredFiles = state.filteredFileList ?? [];
-        if (filteredFiles.isEmpty) {
-          return const EmptyScreen(
-            title: "Search results Not Found",
-            displayIcon: Icons.not_accessible,
-          );
-        }
-        return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.black, Colors.black54],
-            ),
-          ),
-          child: ListView.builder(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-            itemCount: filteredFiles.length,
-            itemBuilder: (context, index) {
-              final song = filteredFiles[index];
-              return AudioFileDisplayCard(
-                song: song,
-                onPlayAudio: (selectedSong) {
-                  final bloc = context.read<MusicBloc>();
-                  bloc.add(SelectSongEvent(song: selectedSong));
-                  bloc.add(ClearFilterSongs());
-                  Navigator.pop(context);
-                },
-              );
-            },
-          ),
+  Widget _buildEmptyState() {
+    return const Center(
+      child: EmptyScreen(
+        title: "Search results Not Found",
+        displayIcon: Icons.not_accessible,
+      ),
+    );
+  }
+
+  Widget _buildSearchResults(List<dynamic> filteredFiles) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+      itemCount: filteredFiles.length,
+      cacheExtent: 100,
+      physics: const AlwaysScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        final song = filteredFiles[index];
+        return AudioFileDisplayCard(
+          song: song,
+          onPlayAudio: (selectedSong) {
+            final bloc = context.read<MusicBloc>();
+            bloc.add(SelectSongEvent(song: selectedSong));
+            bloc.add(ClearFilterSongs());
+            Navigator.pop(context);
+          },
         );
       },
+    );
+  }
+}
+
+// Modify your EmptyScreen widget to be centered properly
+class EmptyScreen extends StatelessWidget {
+  final String title;
+  final IconData displayIcon;
+
+  const EmptyScreen({
+    Key? key,
+    required this.title,
+    required this.displayIcon,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(height: 64),
+        Icon(
+          displayIcon,
+          size: 80,
+          color: Colors.grey,
+        ),
+        const SizedBox(height: 16),
+        Text(
+          title,
+          style: const TextStyle(
+            fontSize: 18,
+            color: Colors.grey,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        const Spacer()
+      ],
     );
   }
 }
